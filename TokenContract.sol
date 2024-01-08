d8f2093f3b2823da8875bf59760c498aace065cd
Adeola David Adelakun <101281102+Adesdesk@users.noreply.github.com>
2024-01-08 11:18:51 +0100

    Update TokenContract.sol

--- TokenContract.sol
+++ TokenContract.sol
@@ -1,1 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NewToken is ERC20, Ownable {
+    constructor()
+        ERC20("NewToken", "NTK")
+        Ownable(msg.sender)
+    {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), to, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        _transfer(from, to, amount);
+        _approve(
+            from,
+            _msgSender(),
+            allowance(from, _msgSender()) - amount
+        );
+        return true;
+    }
+}
 
